2f256159ec213fa98a79c9406ed51607084f0f15
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-26 16:10:55 +0300

    create route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Todos, { loader as todosLoader } from "./pages/Todos";
+import NewTodo, { action as newTodoAction } from "./pages/NewTodo";
+import TodoDetails, {
+  loader as todoDetailsLoader,
+  action as todoDetailsAction,
+} from "./pages/TodoDetails";
+
 function App() {
-  return <div>Hello You!</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Todos />,
+      loader: todosLoader,
+      children: [
+        {
+          path: "new",
+          element: <NewTodo />,
+          action: newTodoAction,
+        },
+        {
+          path: ":id",
+          element: <TodoDetails />,
+          loader: todoDetailsLoader,
+          action: todoDetailsAction,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
